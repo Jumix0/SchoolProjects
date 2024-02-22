86701ce1090063cd3c4a60cd7d1fe49ab5a680f1
Jumix <116796764+Jumix0@users.noreply.github.com>
2024-02-22 09:39:00 +0100

    Simulation of local chat

---
+++ ChatSimulation/ChatP2P/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Server // CHABANIUK ANDRII, 4H, Simulazione Server, 2024-02-15
+             // Simulazione di un server che comunica con i Client.
+{
+    private static Socket listener;
+    private static List<ClientHandler> clients = new List<ClientHandler>();     
+    private static List<string> connectedClients = new List<string>();
+   
+    public static void Start()
+    {
+        // Creazione ed assegnazione di valori ad un server
+        listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        listener.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000));
+        listener.Listen(10); // Specifica il numero massimo di persone che si possono collegare.
+        Console.WriteLine("Server started.");
+
+        while (true)
+        {
+            Socket clientSocket = listener.Accept(); // Ciclo che è sempre in azione(cioé server è sempre in ascolto) di acettare un nuovo client.
+            ClientHandler clientHandler = new ClientHandler(clientSocket);  // Viene creato un clienthandler per ogni nuovo client.
+            clients.Add(clientHandler); // Aggiunge un client nella lista.
+
+            Thread clientThread = new Thread(clientHandler.HandleClient); // Thread che si occupa di comunicazione.
+            clientThread.Start(); // Fa partire il thread.
+        }
+    }
+
+    public static void BroadcastMessage(string message) // Messaggio che viene inviato a tutti client
+    {
+        foreach (var client in clients)
+        {
+            client.Send(message);
+        }
+    }
+
+    public static void BroadcastConnectionStatus(string nickname, bool isConnected) // Messaggio che viene inviato a tutti, riguardo allo stato di conessione di un client.
+    {
+        string statusMessage = "";
+        if (isConnected) // Verifica se c'è stata la connessione o la disconnessione, tramite un boolean.
+        {
+            connectedClients.Add(nickname); // Qualcuno si è connesso.
+            statusMessage = $"{nickname} has joined";
+            foreach (var client in clients)
+            {
+                client.Send(statusMessage);
+            }
+            if (connectedClients.Count == 1) // Verifica se il client è primo a collegarsi nella chat.
+            {
+                BroadcastMessage("You are the first one to join the chat.");
+            }
+            else // In caso se ci sono alte persone collegate, a tutti i client viene aggiornata la lista dei persone collegate.
+            {
+                string clientList = "People already connected to the chat:";
+                foreach (var client in connectedClients)
+                {
+                    clientList += $"\n- {client}";
+                }
+                BroadcastMessage(clientList);
+            }
+        }
+        else // Se if è false, quindi il client si è dissconesso.
+        {
+            statusMessage = $"{nickname} has left";
+            connectedClients.Remove(nickname); // Rimuove il client dalla lista dei client connessi.
+            BroadcastMessage(statusMessage); // Manda il messaggio a tutti che un client si è disconnesso.
+
+            // Aggiorna la listBox sul server
+            UpdateConnectedClientsListBox();
+        }
+    }
+
+    private static void UpdateConnectedClientsListBox() // Si aggiorna la lista dei client connessi.
+    {
+        string clientList = "People currently connected to the chat:";
+        foreach (var client in connectedClients)
+        {
+            clientList += $"\n- {client}";
+        }
+
+        BroadcastMessage(clientList);
+    }
+
+    public static void RemoveClient(ClientHandler client) // Metodo che rimuove un client dalla lista.
+    {
+        clients.Remove(client);
+    }
+}
+
+class ClientHandler
+{
+    private Socket clientSocket;
+    private string nickname;
+    private string ip;
+    private DateTime connectionTime;
+
+    public ClientHandler(Socket clientSocket) // Costruttore della classe.
+    {
+        this.clientSocket = clientSocket;
+        ip = ((IPEndPoint)clientSocket.RemoteEndPoint).Address.ToString();
+        connectionTime = DateTime.Now;
+    }
+
+    public void HandleClient()
+    {
+        byte[] buffer = new byte[1024];
+        int bytesRead = clientSocket.Receive(buffer);
+        nickname = Encoding.ASCII.GetString(buffer, 0, bytesRead).Trim(); // Trim cancella gli spazi vuoti all'inizio o alla fine del messaggio
+                                                                          // e quindi il nickname del client si prende dalla listbox inviato al server.
+        Console.WriteLine($"New client connected: {nickname}, {ip}, {connectionTime}"); 
+        Server.BroadcastConnectionStatus(nickname, true); // Scrive a tutti che si è collegato un client.
+
+        while (true) // Il ciclo infinito che riceve i dati da client.
+        {
+            try // Accetta il messaggio e lo decodifica secondo il protocollo <EOF>
+            {
+                bytesRead = clientSocket.Receive(buffer);
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead).Trim();
+
+                if (message.EndsWith("<EOF>"))
+                {
+                    message = message.Substring(0, message.Length - "<EOF>".Length);
+                }
+
+                Console.WriteLine($"[{nickname}]: {message}");
+
+                Server.BroadcastMessage($"[{nickname}]: {message}");
+            }
+            catch // Eccezione in cui il client interrompe la comunicazione e il try entra nella catch dove esegue l'uscità dal ciclo.
+            {
+                Console.WriteLine($"Client disconnected: {nickname}, {ip}, {DateTime.Now}");
+                Server.RemoveClient(this);
+                Server.BroadcastConnectionStatus(nickname, false);
+                break;
+            }
+        }
+        // C'è stata la disconnessione e quindi il server sospende i due socket di comunicazione e gli chiude.
+        clientSocket.Shutdown(SocketShutdown.Both);
+        clientSocket.Close(); 
+    }
+
+    public void Send(string message) // Manda il messaggio che viene passato al metodo al client tramite socket
+    {
+        byte[] buffer = Encoding.ASCII.GetBytes(message);
+        clientSocket.Send(buffer);
+    }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Server.Start(); // Invia il server.
+    }
+}
---
+++ ChatSimulation/ClientWPFS/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+using System; // using neccessari
+
+namespace ClientWPFS
+{
+    public partial class MainWindow : Window // CHABANIUK ANDRII, 4H, Simulazione Client, 2024-02-15
+    {                                        // Simulazione di un client che comunica con il server.
+        private Socket clientSocket;
+        private string nickname;
+        private Thread receiveMessageThread;  // Thread per fare in modo che il programma non si ferma e continua ad ascoltare i messagi.
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e) // Bottone che si occupa di connettersi al server, dopo che è stato inserito il nome.
+        {
+            nicknameTextBox.Visibility = Visibility.Hidden;
+            disconnectButton.Visibility = Visibility.Visible;
+            messageTextBox.Visibility = Visibility.Visible;
+            sendMessageButton.Visibility = Visibility.Visible;
+            connectButton.Visibility = Visibility.Hidden; // Gestione dei bottoni/textbox dopo che un client si connette.
+            try // Prova a collegarsi al server, se non riesce andrà in catch e scrive il messaggio nella messagebox di un errore.
+            {
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // Crea un socket di comunicazione.
+                clientSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000)); // Si conette al server
+
+                nickname = nicknameTextBox.Text;
+                byte[] nicknameData = Encoding.ASCII.GetBytes(nickname);
+                clientSocket.Send(nicknameData); // Converte il nome in byte e lo manda al server.
+
+                receiveMessageThread = new Thread(ReceiveMessages);
+                receiveMessageThread.Start(); // Thread che gestisce i messaggi recevuti dal server, in modo che non si blocca l'esecuzione del programma.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during connection: {ex.Message}");
+            }
+        }
+
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e) // Bottone che gestisce l'invio del messaggio al server.
+        {
+            try // Prova a collegarsi al server, se non riesce andrà in catch e scrive il messaggio nella messagebox di un errore.
+            {
+                string message = messageTextBox.Text;
+                byte[] messageData = Encoding.ASCII.GetBytes(message + "<EOF>");
+                clientSocket.Send(messageData); // trasforma il messaggio in bytes, aggiunge il protocollo e lo manda.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error sending message: {ex.Message}");
+            }
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e) // Bottone che si occupa di disconessione dal server.
+        {
+            try // Prova a collegarsi al server, se non riesce andrà in catch e scrive il messaggio nella messagebox di un errore.
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close(); // Sospende i socket di comunicazione e gli chiude.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during disconnection: {ex.Message}");
+            }
+            finally // Chiude tutto il programma dopo la disconessione
+            { 
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void ReceiveMessages() // Il metodo che si occupa di ricevere i messaggi
+        {
+            byte[] buffer = new byte[1024]; // Array con i dati
+            while (true)
+            {
+                try
+                {
+                    if (clientSocket == null || !clientSocket.Connected) // Verifica se il client è disconesso, se è disconesso esce dal ciclo e termina.
+                    {
+                        break;
+                    }
+
+                    int bytesRead = clientSocket.Receive(buffer);
+                    string receivedData = Encoding.ASCII.GetString(buffer, 0, bytesRead).Trim();  // Legge i byte e gli trasforma in stringa, il metodo .Trim() --> cancella i spazzi vuoti all'inizio della stringa e alla fine
+
+                    if (receivedData.StartsWith("People already connected to the chat:") || receivedData.StartsWith("People currently connected to the chat:")) // Se arrivano i messaggi che iniziano con quelle parole, 
+                    {                                                                                                                                           // quindi è da aggiornare listbox.
+                        Dispatcher.Invoke(() => // la possibilità che offre WPF .NET, di eseguire una parte del codice con le sue risorse. ((https://learn.microsoft.com/it-it/dotnet/api/system.windows.threading.dispatcher.invoke?view=windowsdesktop-8.0))
+                        {
+                            connectedClientsListBox.Items.Clear();
+                            connectedClientsListBox.Items.Add(receivedData); // Pulisce e mette i client conessi nella listbox dedicata.
+                        });
+                    }
+                    else
+                    {
+                        string message = receivedData.TrimEnd(); // Prende il messagio e toglie lo spazio vuoto alla fine se c'è.
+                        if (message.EndsWith("<EOF>"))  // Controlla il messaggio, toglie <EOF> e lo aggiunge nella listbox. EOF sta per end of stream
+                        {
+                            message = message.Substring(0, message.Length - "<EOF>".Length);
+                            Dispatcher.Invoke(() =>
+                            {
+                                messageListBox.Items.Add(message);
+                            });
+                        }
+                        if (message.EndsWith("<CHC>")) // Controlla il messaggio, toglie <CHC> e lo aggiunge nella listbox. CHC sta per client has connected
+                        {
+                            message = message.Substring(0, message.Length - "<CHC>".Length);
+                            Dispatcher.Invoke(() =>
+                            {
+                                messageListBox.Items.Add(message);
+                            });
+                        }
+                        else if (message.EndsWith("<CHD>")) // Controlla il messaggio, toglie <CHD> e lo aggiunge nella listbox. CHD sta per client has disconnected
+                        {
+                            message = message.Substring(0, message.Length - "<CHD>".Length);
+                            Dispatcher.Invoke(() =>
+                            {
+                                messageListBox.Items.Add(message);
+                            });
+                        }
+                        else
+                        {
+                            Dispatcher.Invoke(() =>
+                            {
+                                messageListBox.Items.Add(message);  // Aggiunge il messaggio nella listbox.
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error receiving messages: {ex.Message}");
+                    break;
+                }
+            }
+        }
+    }
+}
